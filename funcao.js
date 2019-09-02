9812e3b229ff661e5dfda8540699b065f3b9113c
buildnfix <54026421+buildnfix@users.noreply.github.com>
2019-09-01 22:14:47 -0300

    Add files via upload

---
+++ funcao.js
@@ -0,0 +1,14 @@
+$(function(){
+	$("#estado").change(function(){
+		var id= $(this).val();
+		$.ajax({	
+			type: "POST",
+			url: "exibe_cidade.php?id="+id,
+			dataType:"text",
+			success:function(res){
+				$("#cidades").children(".cidades").remove();
+				$("#cidades").append(res);
+			}
+		});
+	});	
+});	
